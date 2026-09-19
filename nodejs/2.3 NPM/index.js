56e5b95b7f55f0df6806dc528f0339ce2fd37550
Dayal Joy <dayaljoy.official@gmail.com>
2026-09-19 22:19:15 +0530

    .

---
+++ nodejs/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// // var generateName = require('sillyname');
+// import generateName from 'sillyname';
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}`);
+import superheros from 'superheros';
+const mySuperHero = superheros.random();
+
+
+console.log(`And my favorite super hero is ${mySuperHero}`);
